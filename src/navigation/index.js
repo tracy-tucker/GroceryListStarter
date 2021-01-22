ffa15f056063731718001c0157f98f8ef4b19100
treuther <tracyreuther@gmail.com>
2021-01-22 12:59:03 -0600

    icons added to navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,6 +7,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CurrentList from '../screens/CurrentList';
 import ItemDetails from '../screens/ItemDetails';
 import FavoritesList from '../screens/FavoritesList';
+import { color } from 'react-native-reanimated';
+import { View } from 'react-native';
 
 const Stack = createStackNavigator();
 const Tabs = createBottomTabNavigator();
@@ -15,7 +17,6 @@ function MyStack() {
   return (
     <Stack.Navigator initialRouteName="Shopping List">
       <Stack.Screen name="Shopping List" component={CurrentList} />
-      {/* <Stack.Screen name="ItemDetails" component={ItemDetails} options={{title: 'Item'}} /> */}
       <Stack.Screen name="ItemDetails" component={ItemDetails} options={({route}) => ({ title: route.params.itemCallback.name})} />
     </Stack.Navigator>
   )
@@ -31,9 +32,31 @@ function MyFavs() {
 
 function MyTabs() {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name="Shopping List" component={MyStack} />
-      <Tabs.Screen name="Favorites List" component={MyFavs} />
+    <Tabs.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Current') {
+          iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+        } else if (route.name === 'Favorites') {
+          iconName = focused ? 'ios-star' : 'ios-star-outline';
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: 'purple',
+      inactiveTintColor: 'gray',
+    }}
+    >
+      <Tabs.Screen
+        name="Current"
+        component={MyStack}
+      />
+      <Tabs.Screen name="Favorites" component={MyFavs} />
     </Tabs.Navigator>
   )
 }
